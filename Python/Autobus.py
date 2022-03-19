f3fda150bfe213dc937b59296929a21869b9edd9
alemr <alemr0104@gmail.com>
2022-03-18 20:23:57 -0600

    creadno archivos aparte

---
+++ Python/Autobus.py
@@ -0,0 +1,25 @@
+class Autobus():
+    __kilometraje = None
+    __color = None
+
+    def getKilometraje(self):
+        return self.__kilometraje
+
+    def getColor(self):
+        return self.__color
+
+    def setKilometraje(self, kilometraje):
+        self.__kilometraje = kilometraje
+
+    def setColor(self, color):
+        self.__color = color
+
+
+def main():
+    miAutobus = Autobus()
+    print("El kilometraje es: ", miAutobus.getKilometraje())
+    print("El color es: ", miAutobus.getColor())
+
+
+if __name__ == '__main__':
+    main()
